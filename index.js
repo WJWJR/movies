15b9861f9ab242f3a618b609b1c515e9851a6605
William J Weekes Jr <WilliamWeekes@Williams-MacBook-Pro.local>
2017-03-28 06:09:36 -0400

    Styled page, Trying to get the search field working

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 function useJsonOnPage (json) {
-     console.log(json);
+    // console.log(json);
      let title = document.querySelector('.movieTitle');
      title.textContent = json.title;
-     /*let avatar = document.querySelector('img');
-     avatar.src = 'https://image.tmdb.org/t/p/w500' + json.results[1].poster_path ;*/
+     let poster_path = document.querySelector('img');
+     poster_path.src = 'https://image.tmdb.org/t/p/w500' + json.poster_path;
      let overview = document.querySelector('.description');
      overview.textContent = json.overview;
      let release_date = document.querySelector('.openingDate');
@@ -20,3 +20,25 @@ function useJsonOnPage (json) {
    fetch("https://api.themoviedb.org/3/movie/263115?api_key=" + key + " ")
      .then(response => response.json())
      .then(useJsonOnPage);
+
+
+//Show User Inputing Movie Titles
+function showUserInput(event) {
+  //let input = event.target.innerText;
+  input.innerHTML;
+  console.log(showUserInput);
+}
+
+let input = document.querySelector('#movies');
+input.innerHTML;
+
+// Show User Clicking Search Button
+function userClickedButton(event) {
+
+}
+
+let buttonClicked = document.querySelector('#search');
+console.log(buttonClicked)
+buttonClicked.addEventListener('click', userClickedButton);
+//console.log(buttonClicked)
+//fetch("https://api.themoviedb.org/3/search/movie?api_key=" + key + " ")&query=+"input.value()")
